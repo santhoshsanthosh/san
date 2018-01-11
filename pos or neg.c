ae8c91cd54a3b4318202150e593fe472cde17cb3
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-01-11 10:45:46 +0000

    Rename positive or negative.c to pos or neg.c

---
+++ pos or neg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("%d enter the number");
+if(a>0)
+printf("%d the number is positive");
+elseif(a<0)
+printf("%d the number is negative");
+else
+printf("%d the number is zero");
+}
